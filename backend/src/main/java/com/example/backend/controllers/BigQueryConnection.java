526449f66066ef37b325f3e7ea378f4fd21fafa8
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-07-22 14:11:05 -0500

    Cleaning up code with comments and links

--- backend/src/main/java/com/example/backend/controllers/BigQueryConnection.java
+++ backend/src/main/java/com/example/backend/controllers/BigQueryConnection.java
@@ -23,13 +23,33 @@ import com.google.cloud.bigquery.JobInfo;
 import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.cloud.bigquery.TableResult;
 
+/*
+HELPFUL LINKS:
+    (1) Query a public dataset with BigQuery client libraries
+        https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries
+
+    (2) Authenticating with a service account key file for BigQuery
+        https://cloud.google.com/bigquery/docs/authentication/service-account-file
+
+    (3) Convert BigQuery output easily to JSON
+        https://stackoverflow.com/questions/44150064/how-to-get-query-result-in-json-form-using-java-api-of-google-bigquery
+*/ 
+
 @RestController
 public class BigQueryConnection {
+    // fields
     BigQuery privateBigQuery;
     BigQuery publicBigQuery;
 
+    // ****************************************************************
+    // ********************** CONSTRUCTOR(S) **************************
+    // ****************************************************************
+
+    // Constructor to instantiate public and private bigquery instances for later use
     public BigQueryConnection() throws IOException {
-        // TODO(developer): Replace these variables before running the sample.
+        // (1) INSTANTIATE PRIVATE CLIENT
+        // https://cloud.google.com/bigquery/docs/authentication/service-account-file
+
         String projectId = "profitable-infra-consumption";
         File credentialsPath = new File("C:\\Users\\fwintz\\Downloads\\profitable-infra-consumption-c781e9bccc56.json");
         // File credentialsPath = new File("C:/Users/fwintz/Downloads/profitable-infra-consumption-c781e9bccc56.json");
@@ -56,11 +76,27 @@ public class BigQueryConnection {
             System.out.printf("%s%n", dataset.getDatasetId().getDataset());
         }
 
-        // instantiate public client
+        // (1) INSTANTIATE PUBLIC CLIENT
         publicBigQuery = BigQueryOptions.getDefaultInstance().getService();
     }
 
-    public String getJSONFromQuery(String query,String queryString) throws Exception {
+
+
+
+    // ****************************************************************
+    // ******************** HELPER FUNCTION(S) ************************
+    // ****************************************************************
+    
+    // helper function to get JSON string from a given query
+    public String getJSONFromQuery(String oldQuery, String publicOrPrivate) throws Exception {
+        // modify query to make json configuration easier
+        // https://stackoverflow.com/questions/44150064/how-to-get-query-result-in-json-form-using-java-api-of-google-bigquery
+        String query = "WITH MyTable AS (" + oldQuery +
+                            ") SELECT TO_JSON_STRING(t) AS json " +
+                            "FROM MyTable AS t";
+
+        // create query job configuration based on input
+        // https://cloud.google.com/bigquery/docs/quickstarts/quickstart-client-libraries
         QueryJobConfiguration queryConfig =
             QueryJobConfiguration.newBuilder(query)
             // Use standard SQL syntax for queries.
@@ -71,13 +107,12 @@ public class BigQueryConnection {
         // Create a job ID so that we can safely retry.
         JobId jobId = JobId.of(UUID.randomUUID().toString());
         Job queryJob;
-        if (queryString.equals("public")) {
+        if (publicOrPrivate.equals("public")) {
             queryJob = publicBigQuery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
         } else {
             queryJob = privateBigQuery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
         }
         
-    
         // Wait for the query to complete.
         queryJob = queryJob.waitFor();
     
@@ -93,80 +128,42 @@ public class BigQueryConnection {
         // Get the results.
         TableResult result = queryJob.getQueryResults();
         
-        // Print all pages of the results.
+        // transform TableResult into Iterable object
         Iterable<FieldValueList> iterable = result.iterateAll();
         
-        List<String> output = new ArrayList<String>();
+        // convert iterable result into JSON string joiner
         StringJoiner strJoin = new StringJoiner(",");
-        // iterable.forEach(output::add);
         iterable.forEach(s-> {
-            output.add(s.get("json").getStringValue());
             strJoin.add(s.get("json").getStringValue());
         });
-        
-        // // Print all pages of the results.
-        // for (FieldValueList row : result.iterateAll()) {
-        //   // String type
-        //   String url = row.get("url").getStringValue();
-        //   String viewCount = row.get("view_count").getStringValue();
-        //   System.out.printf("%s : %s views\n", url, viewCount);
-        // }
-        // return result;
-        // for (FieldValueList row : result.iterateAll()) {
-        //     return row.get("json").getStringValue();
-        // }
+
+        // modify joined strings by turning them into a list of jobs in JSON format
         return "[" + strJoin.toString() + "]";
     }
 
+
+
+
+    // ****************************************************************
+    // ********************* API ENDPOINTS ****************************
+    // ****************************************************************
+
     @GetMapping("/GET/public-data")
     public String getPublicData() throws Exception {
-        // QueryJobConfiguration queryConfig =
-        //     QueryJobConfiguration.newBuilder(
-        //             "SELECT CONCAT('https://stackoverflow.com/questions/', "
-        //                 + "CAST(id as STRING)) as url, view_count "
-        //                 + "FROM `bigquery-public-data.stackoverflow.posts_questions` "
-        //                 + "WHERE tags like '%google-bigquery%' "
-        //                 + "ORDER BY view_count DESC "
-        //                 + "LIMIT 10")
-        //     // Use standard SQL syntax for queries.
-        //     // See: https://cloud.google.com/bigquery/sql-reference/
-        //     .setUseLegacySql(false)
-        //     .build();
-        // String query = ("SELECT * FROM `ctg-storage.bigquery_billing_export.gcp_billing_export_v1_01150A_B8F62B_47D999` " +
-        //                 "WHERE DATE(_PARTITIONTIME) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) " + 
-        //                 "LIMIT 100");
+        // gets full table data for publically available data (up to 100 jobs)
         String newQuery = (
-                        "WITH MyTable AS (SELECT * FROM `ctg-storage.bigquery_billing_export.gcp_billing_export_v1_01150A_B8F62B_47D999` " +
+                        "SELECT * FROM `ctg-storage.bigquery_billing_export.gcp_billing_export_v1_01150A_B8F62B_47D999` " +
                         "WHERE DATE(_PARTITIONTIME) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) " + 
-                        "LIMIT 100) " +
-                        "SELECT TO_JSON_STRING(t) AS json " +
-                        "FROM MyTable AS t");
+                        "LIMIT 100");
         return getJSONFromQuery(newQuery,"public");
     }
 
     @GetMapping("/GET/private-data")
     public String getPrivateData() throws Exception {
-        // QueryJobConfiguration queryConfig =
-        //     QueryJobConfiguration.newBuilder(
-        //             "SELECT CONCAT('https://stackoverflow.com/questions/', "
-        //                 + "CAST(id as STRING)) as url, view_count "
-        //                 + "FROM `bigquery-public-data.stackoverflow.posts_questions` "
-        //                 + "WHERE tags like '%google-bigquery%' "
-        //                 + "ORDER BY view_count DESC "
-        //                 + "LIMIT 10")
-        //     // Use standard SQL syntax for queries.
-        //     // See: https://cloud.google.com/bigquery/sql-reference/
-        //     .setUseLegacySql(false)
-        //     .build();
+        // gets full table data for our private data (up to 100 jobs)
         String query = ("SELECT * FROM `profitable-infra-consumption.all_billing_data.gcp_billing_export_v1_0124FF_8C7296_9F0D41` " +
                         "WHERE DATE(_PARTITIONTIME) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY) " + 
                         "LIMIT 100");
         return getJSONFromQuery(query,"private");
     }
-
-    @GetMapping("/POST")
-    public String getHelloWorld() {
-        return "Hello World!";
-    }
-    
 }
