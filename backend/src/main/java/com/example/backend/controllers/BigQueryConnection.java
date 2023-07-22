929637c67e4ef72ca9ff4cd71b550d3d89b3b0e7
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-07-22 14:12:03 -0500

    Removing unnecessary imports

--- backend/src/main/java/com/example/backend/controllers/BigQueryConnection.java
+++ backend/src/main/java/com/example/backend/controllers/BigQueryConnection.java
@@ -3,8 +3,6 @@ package com.example.backend.controllers;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringJoiner;
 import java.util.UUID;
 
