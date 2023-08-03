b760206d96f59eeb42745ad868d899c3db4c1206
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-08-03 14:55:31 -0500

    Small formatting changes to API Calls file

--- frontend/src/utils/APICalls.tsx
+++ frontend/src/utils/APICalls.tsx
@@ -12,37 +12,31 @@ export const getAllData = (range:string,allColumns:boolean) =>  {
     } else {
         queries = "?range="+range+"&allColumns="+allColumns
     }
-    return axios.get(baseURL+endpoint+queries)
+    return axios.get<string[]>(baseURL+endpoint+queries)
 }
 
 export const getCostByMonth = (range:string) => {
     let endpoint : string = "api/cost-by-month"
-    let queries : string
+    let queries : string = ""
     if (range === "") {
-        queries = ""
-    } else {
         queries = "?range="+range
     }
-    return axios.get(baseURL+endpoint+queries)
+    return axios.get<string[]>(baseURL+endpoint+queries)
 }
 
 export const getCostByProject = (range:string) => {
     let endpoint : string = "api/cost-by-project"
-    let queries : string
-    if (range === "") {
-        queries = ""
-    } else {
+    let queries : string = ""
+    if (range !== "") {
         queries = "?range="+range
     }
     return axios.get<string[]>(baseURL+endpoint+queries)
 }
 export const getCostByService = (range:string) => {
     let endpoint : string = "api/cost-by-service"
-    let queries : string
-    if (range === "") {
-        queries = ""
-    } else {
+    let queries : string = ""
+    if (range !== "") {
         queries = "?range="+range
     }
-    return axios.get(baseURL+endpoint+queries)
+    return axios.get<string[]>(baseURL+endpoint+queries)
 }
